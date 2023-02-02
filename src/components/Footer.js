3f3fa93e9a5fc9738da77f1e7a447a848bba4711
Jason Ruben <jsnrbn01@gmail.com>
2023-02-02 11:26:38 +0800

    added link on brands

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <header className="footer d-flex align-items-end">
             <div className="container-lg d-flex justify-content-center align-items-end">
                 <div className="brand d-flex flex-column text-center gap-2">
-                    <h1 className="m-0 p-0">.DEV</h1>
+                    <h1 className="m-0 p-0"><a href="/" className="text-decoration-none">.DEV</a></h1>
                     <p className="pb-3"><i className="bi bi-c-circle"></i> All Rights Reserved | Jason Santillan Ruben</p>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header className="header">
             <div className="container-lg d-flex justify-content-between align-items-center">
                 <div className="brand d-flex align-self-end">
-                    <h1 className="m-0 p-0">.DEV</h1>
+                    <h1 className="m-0 p-0"><a href="/" className="text-decoration-none">.DEV</a></h1>
                 </div>
                 <nav className="nav">
                     <ul className="nav-links d-flex gap-5 m-0 ">
