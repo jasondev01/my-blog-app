760ef9437fb991ef01d5ef3e54d4622e9dff25bd
Jason Ruben <jsnrbn01@gmail.com>
2023-06-02 20:43:04 +0800

    deleted some text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className="container-lg d-flex justify-content-center align-items-end">
                 <div className="brand d-flex flex-column text-center gap-2">
                     <h1 className="m-0 p-0"><a href="/" className="text-decoration-none">.DEV</a></h1>
-                    <p className="pb-3"><i className="bi bi-c-circle"></i> All Rights Reserved 2023 | Jason Santillan Ruben</p>
+                    <p className="pb-3"><i className="bi bi-c-circle"></i> All Rights Reserved 2023 | Jason Ruben</p>
                 </div>
             </div>
         </header>
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
                <div className="row">
                     <div className="col-8">
                         <h3 className="my-2">Hey, there! I am</h3>
-                        <h1 className="my-3">JASON SANTILLAN RUBEN</h1>
+                        <h1 className="my-3">JASON RUBEN</h1>
                         <h2 className="my-2">And I am currently learning ReactJS</h2>
                         <p className="my-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus porro sapiente, assumenda voluptates fuga dignissimos rem dolor maxime voluptas accusamus illo itaque veniam possimus reprehenderit impedit dolorum aut, consequuntur id?</p>
                         <div className="buttons d-flex gap-3 py-1">
